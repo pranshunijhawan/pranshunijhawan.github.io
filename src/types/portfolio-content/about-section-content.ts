6fe09f9fd4a3da53ee49a93978362804af579307
Pranshu Nijhawan <Pranshu_Nijhawan@mckinsey.com>
2022-05-29 13:10:23 +0530

    About Content Fix

--- src/types/portfolio-content/about-section-content.ts
+++ src/types/portfolio-content/about-section-content.ts
@@ -3,7 +3,7 @@ import { AboutSectionProps } from "../../sections/about/About";
 const aboutSectionContent: AboutSectionProps = {
     sectionTitle: 'About Me',
     content: [
-        'A Dynamic IT Professional responsible for assisting in the establishment of an IT Architecture practice, and the adoption of an architecture and standards methodology.',
+        'A Dynamic IT Professional responsible for assisting in the establishment of an IT Architecture practice, and the adoption of an architecture and standard methodology.',
         'Passionate about designing highly stable and scalable Software Solutions that transcend high performance ranging from traditional Mobile, Web to IoT Management System.',
         'If you stick till the end of this page, you can have a glimpse of what I\'m most proud of, the skills I possess and the Experience I hold.',
         'Note: This is just a glimpse, I\'m constantly improving and evolving! 👨‍💻'
