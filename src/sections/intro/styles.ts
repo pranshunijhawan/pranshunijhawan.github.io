2a3d787f32747a6da4cd4144ee1b32a069d21a82
Pranshu Nijhawan <Pranshu_Nijhawan@mckinsey.com>
2022-06-11 13:52:25 +0530

    Removed React Code

--- src/sections/intro/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { slideInEnter, slideOutBackExit } from "../../types/common-css";
-
-export const IntroContainer = styled.div<{
-    enter: boolean
-}>`
-    ${({ enter }) => enter ? slideInEnter : slideOutBackExit }
-    opacity: ${({ enter }) => enter ? 0 : 1 };
-`
--- src/ui-components/Section.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { section } from "../types/common-css";
-
-export const Section = styled.section`
-    ${section}
-    transition: opacity 1s, visibility 1s;
-    scroll-margin-top: var(--mb-2);
-`
